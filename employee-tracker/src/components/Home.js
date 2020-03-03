62dc3a5e8c260a0108a25e0c4efc9231d17f10c6
Katie Durga <katie.durga@gmail.com>
2020-03-03 15:38:29 -0500

    layout and display of employees

--- employee-tracker/src/components/Home.js
+++ employee-tracker/src/components/Home.js
@@ -40,7 +40,6 @@ class About extends Component {
               {this.state.results.map(user =>
               <li className='list-group-item' key={user.id.value}><img alt={user.name.last} src={user.picture.medium}/> &nbsp; {user.name.last}, &nbsp; {user.name.first} &nbsp; {user.email}</li>
               )}
-              
             </ul>
           </div>
         </div>
