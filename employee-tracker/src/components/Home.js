3243a80dad8a286aea51d490598cc5b7840aa4c9
Katie Durga <katie.durga@gmail.com>
2020-03-04 10:22:36 -0500

    took search back out of its own component

--- employee-tracker/src/components/Home.js
+++ employee-tracker/src/components/Home.js
@@ -1,18 +1,25 @@
 import React, { Component } from "react";
 import API from "./utils/API";
-import SearchForm from "./Search";
+// import SearchForm from "./Search";
 
 
 class About extends Component {
+  //set initial state
   state = {
     search: "",
     results: []
   };
 
+  //sets state of search to the target valiue
+  updateSearch(event) {
+    this.setState({search: event.target.value.substr(0, 20)})
+  }
+
   componentDidMount() {
     this.users();
   }
 
+  //gets api data
   users = () => {
     API.users()
     
@@ -23,22 +30,37 @@ class About extends Component {
 
 
   render() {
+    //filters users
+    let filteredUsers = this.state.results.filter(
+      (user) => {
+        return user.name.last.indexOf(this.state.search) !== -1
+      }
+    )
     return (
       <div>
         <div className="jumbotron jumbotron-fluid">
           <div className="container">
-          <div className="jumbotron jumbotron-fluid">
-            <div className="container">
-              <h1 className="display-4">Employee Tracker</h1>
+            <div className="jumbotron jumbotron-fluid">
+              <div className="container">
+                <h1 className="display-4">Employee Tracker</h1>
+              </div>
             </div>
-          </div>
-          <SearchForm
-            search={this.state.search}
-            handleFormSubmit={this.handleFormSubmit}
-            handleInputChange={this.handleInputChange}
-          />
+            <form>
+              <div className="form-group">
+                <label htmlFor="search">Search:</label>
+                <input
+                  name="search"
+                  type="text"
+                  className="form-control"
+                  placeholder="Search for an employee by last name"
+                  id="search"
+                  onChange={this.updateSearch.bind(this)}
+                  value={this.state.search}
+                />
+              </div>
+            </form>
             <ul className='list-group'>
-              {this.state.results.map(user =>
+              {filteredUsers.map(user =>
               <li className='list-group-item' key={user.id.value}><img alt={user.name.last} src={user.picture.medium}/> &nbsp; {user.name.last}, &nbsp; {user.name.first} &nbsp; {user.email}</li>
               )}
             </ul>
